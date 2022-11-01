be7da7ee9fb07e15378c7a13351301c4bc238a67
RamchandraPopatMulik <mulikram2016@gmail.com>
2022-11-01 13:56:42 +0530

    Added file

---
+++ Sanke_And_Ladder_Problem/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Sanke_And_Ladder_Problem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Snake and Ladder Problem !!");
+            Snake snake = new Snake();
+            snake.player();
+        }
+    }
+}
---
+++ Sanke_And_Ladder_Problem/Snake.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sanke_And_Ladder_Problem
+{
+    internal class Snake
+    {
+        public void player()
+        {
+            int Num_Player=1;
+            int Start_Position=0;
+
+            Console.WriteLine($"Number Of Player is :{Num_Player}");
+            Console.WriteLine($"Start Position is :{Start_Position}");
+        }
+    }
+}
