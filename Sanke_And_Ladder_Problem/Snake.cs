9c30ff963af74e62e9ad8ce2534effa8fc6ae3d9
RamchandraPopatMulik <mulikram2016@gmail.com>
2022-11-02 14:21:07 +0530

    Addded file

--- Sanke_And_Ladder_Problem/Snake.cs
+++ Sanke_And_Ladder_Problem/Snake.cs
@@ -14,10 +14,13 @@ namespace Sanke_And_Ladder_Problem
         public void player()
         {
             int position1 = 0;
-            int count=0;
-            
+            int position2 = 0;
+            int count1 = 0;
+            int count2 = 0;
+
             Random random = new Random();
-            while (position1 != 100)
+
+            while (position1 != 100 || position2 !=100)
             {
                 int MovingCheck = random.Next(1, 6);
                 int StepCheck = random.Next(1, 3);
@@ -27,15 +30,15 @@ namespace Sanke_And_Ladder_Problem
                 switch (StepCheck)
                 {
                     case Ahead_Move:
- 
-                            position1 = position1 + MovingCheck;
-                            Console.WriteLine("Ahead Move");
-                        
+
+                        position1 = position1 + MovingCheck;
+                        Console.WriteLine("Ahead Move");
+
                         break;
                     case Behind_Move:
 
-                            position1 = position1 - MovingCheck;
-                            Console.WriteLine("Behind Move");
+                        position1 = position1 - MovingCheck;
+                        Console.WriteLine("Behind Move");
 
                         break;
                     case No_Move:
@@ -45,20 +48,61 @@ namespace Sanke_And_Ladder_Problem
 
                         break;
                 }
-                if(position1 < 0)
+                if (position1 < 0)
                 {
                     Console.WriteLine("Restart the Game");
                     position1 = 0;
                     continue;
                 }
-                if(position1==0)
+                if (position1 == 0)
                 {
                     Console.WriteLine("Player 1st Wins the Game");
                     break;
                 }
-               count++;
+                count1++;
+
+                int MovingCheck1 = random.Next(1, 6);
+                int StepCheck1 = random.Next(1, 3);
+                Console.WriteLine($"Roll Die is : {MovingCheck1}");
+                Console.WriteLine($"Moving Check is : {StepCheck1}");
+
+                switch (StepCheck1)
+                {
+                    case Ahead_Move:
+
+                        position2 = position2 + MovingCheck1;
+                        Console.WriteLine("Ahead Move");
+
+                        break;
+                    case Behind_Move:
+
+                        position2 = position2 - MovingCheck1;
+                        Console.WriteLine("Behind Move");
+
+                        break;
+                    case No_Move:
+
+                        position2 = position2 + MovingCheck1;
+                        Console.WriteLine("No Move");
+
+                        break;
+                }
+                if (position2 < 0)
+                {
+                    Console.WriteLine("Restart the Game");
+                    position2 = 0;
+                    continue;
+                }
+                if (position2 == 0)
+                {
+                    Console.WriteLine("Player 1st Wins the Game");
+                    break;
+                }
+                count2++;
             }
-            Console.WriteLine("Number Of Times Die Win :"+ count);
+                Console.WriteLine("Player 1st Dies: "+count1);
+                Console.WriteLine("Player 2nd Dies: "+count2);
+            
         }
     }
 }
