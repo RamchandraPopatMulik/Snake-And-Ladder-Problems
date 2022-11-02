7a68495ddc62c4d1ce0d4fa73c0cc61a20461d91
RamchandraPopatMulik <mulikram2016@gmail.com>
2022-11-02 12:42:39 +0530

    Addded file

--- Sanke_And_Ladder_Problem/Snake.cs
+++ Sanke_And_Ladder_Problem/Snake.cs
@@ -13,37 +13,39 @@ namespace Sanke_And_Ladder_Problem
         public const int No_Move = 3;
         public void player()
         {
-            int position = 0;
-           Random random = new Random();
-           int MovingCheck = random.Next(1,6);
-            int StepCheck = random.Next(1,3);
-           Console.WriteLine($"Roll Die is : {MovingCheck}");
-            Console.WriteLine($"Moving Check is : {StepCheck}");
+            int position1 = 0;
 
-            switch(StepCheck)
+            Random random = new Random();
+            while (position1 != 100)
             {
-                case Ahead_Move:
-                    if(position < 100)
-                    {
-                        position = position + StepCheck;    
-                        Console.WriteLine("Ahead Move");
-                    }
-                break;
-                case Behind_Move:
-                    if (position > 0)
-                    {
-                        position = position - StepCheck;
-                        Console.WriteLine("Behind Move");
-                    }
-                    break;
-                case No_Move:
-                   
-                        position = position + StepCheck;
+                int MovingCheck = random.Next(1, 6);
+                int StepCheck = random.Next(1, 3);
+                Console.WriteLine($"Roll Die is : {MovingCheck}");
+                Console.WriteLine($"Moving Check is : {StepCheck}");
+
+                switch (StepCheck)
+                {
+                    case Ahead_Move:
+ 
+                            position1 = position1 + MovingCheck;
+                            Console.WriteLine("Ahead Move");
+                        
+                        break;
+                    case Behind_Move:
+
+                            position1 = position1 - MovingCheck;
+                            Console.WriteLine("Behind Move");
+
+                        break;
+                    case No_Move:
+
+                        position1 = position1 + MovingCheck;
                         Console.WriteLine("No Move");
 
-                break;
+                        break;
+                }
+                Console.WriteLine("Step Count is :" + position1);
             }
-            Console.WriteLine("Step Count is :"+position);
         }
     }
 }
